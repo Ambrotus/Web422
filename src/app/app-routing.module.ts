bc19588f5e8ed2a17f5937e3405a2a33b2743eaa
Ambrotus <edrith@gmail.com>
2020-04-10 13:08:55 -0400

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,9 @@ import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './blog/blog.component';
 import { PostComponent } from './post/post.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
+import { PostsTableComponent } from './posts-table/posts-table.component';
+import { NewPostComponent } from './new-post/new-post.component';
 
 //https://web422.ca/notes/angular-routing
 const routes: Routes = [
@@ -12,6 +15,9 @@ const routes: Routes = [
   { path: 'blog', component: BlogComponent },
   { path: 'post/:id', component: PostComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'admin', component: PostsTableComponent },
+  {path: 'admin/post/:id', component: EditPostComponent },
+  {path: 'admin/newPost', component: NewPostComponent},
   { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
